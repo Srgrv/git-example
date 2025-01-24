c124f6494d0280f30a9621c6a33da3dbbfa32164
Sergey Georgiev <sgrgv_art@mail.ru>
2025-01-24 17:12:45 +0300

    feat: Добавил медизапрос, который проверяет hover на мобильных и десктопных устройствах

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -19,7 +19,7 @@ const Projects: React.FC = () => {
   const projects = useAppSelector((state: RootState) => state.projects);
 
   return (
-    <div className="flex flex-col max-w-4xl container mx-auto p-8 ">
+    <div className="flex flex-col max-w-4xl container mx-auto p-8">
       <h1 className="bg-blue-300 rounded-2xl text-center mb-8 text-4xl font-bold text-gray-800 ">
         Мои проекты
       </h1>
@@ -30,7 +30,7 @@ const Projects: React.FC = () => {
             key={project.id}
             className="rounded-2xl md:w-[calc(33.33%-8px)] text-justify sm:w-[calc(50%-8px)]"
           >
-            <Card className="bg-transparent border-none  group hover:bg-blue-300 shadow-[10px_10px_15px_rgba(0,0,0,0.2)] transform hover:scale-105 transition-transform duration-500 will-change-transform">
+            <Card className="bg-transparent border-none group hover-hover:hover:bg-blue-300 shadow-[10px_10px_15px_rgba(0,0,0,0.2)] hover-hover:transform hover-hover:hover:scale-105 hover-hover:transition-transform hover-hover:duration-500 hover-hover:will-change-transform">
               <CardHeader>
                 <CardTitle>{project.title}</CardTitle>
                 <CardDescription>{project.description}</CardDescription>
@@ -40,16 +40,16 @@ const Projects: React.FC = () => {
                   {project.tags.map((tag, index) => (
                     <Badge
                       variant="secondary"
-                      className="text-blue-800 bg-blue-300 hover:bg-blue-300 group-hover:bg-white"
+                      className="text-blue-800 bg-blue-300 hover-hover:group-hover:bg-white"
                       key={index}
                     >
                       {tag}
                     </Badge>
                   ))}
                 </div>
-                <div className="flex text-blue-600 group-hover:text-black justify-center items-center">
+                <div className="flex text-blue-600 hover-hover:group-hover:text-black justify-center items-center">
                   Подробнее{" "}
-                  <ArrowRight className="ml-2 size-4 transform transition-transform duration-500 group-hover:translate-x-2" />
+                  <ArrowRight className="ml-2 size-4 hover-hover:transform hover-hover:transition-transform hover-hover:duration-500 hover-hover:group-hover:translate-x-2" />
                 </div>
               </CardContent>
             </Card>
